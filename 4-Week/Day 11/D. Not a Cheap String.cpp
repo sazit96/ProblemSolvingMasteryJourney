df5b4b31982b36a18ce7343cab07c2ef71f30e05
sazit96 <sazitislam96@gmail.com>
2023-07-06 23:25:09 +0600

    update

---
+++ 4-Week/Day 11/D. Not a Cheap String.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> 
+using namespace std;
+int main ()
+{
+    int t; cin >> t; 
+    while (t--)
+    {
+        string w; cin >> w;
+        int p; cin >> p;
+        
+    }
+    return 0;
+}
