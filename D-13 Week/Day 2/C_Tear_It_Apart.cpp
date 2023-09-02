bd449b39e533a07bdef6dd4c78eaf3480cb05d20
sazit96 <sazitislam96@gmail.com>
2023-09-02 23:31:38 +0600

    update week 13 day 2

---
+++ D-13 Week/Day 2/C_Tear_It_Apart.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s; 
+        int n=s.size();
+        int p[n];
+        for(int i=0;i<n;i++)
+         p[i]=s[i] - 'a';
+        int ans=0;
+        int cnt=0;
+        int f_ans=1e7;
+        int g_ans=0;
+        for(int i=0;i<26;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(p[j]==i || j== n-1) 
+                {
+                    g_ans=0;
+                    if(p[j]!=i) cnt++;
+                    while(cnt!=0)
+                    {
+                        cnt/=2;
+                        g_ans++;
+                    }
+                    ans=max(ans,g_ans);
+                }
+                else cnt++;
+            }
+            f_ans=min(ans,f_ans);
+            ans=0;
+        }
+        cout << f_ans << "\n";
+    }
+    return 0;
+}
