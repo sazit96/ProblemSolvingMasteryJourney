e9ed081a3f3664fc6a5ac616f15866ee51216085
sazit96 <sazitislam96@gmail.com>
2023-09-23 21:08:39 +0600

    update week 16 day 2

---
+++ G-16-Week/Day 2/N_Numbers_Histogram.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    string s; cin >> s; 
+    int n; cin >> n; 
+    for(int i=0;i<n;i++)
+    {
+        int cnt; cin >> cnt; 
+        for(int j=0;j<cnt;j++) 
+         cout << s ;
+        cout << "\n";
+    }
+    return 0;
+}
